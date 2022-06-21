4bbb0170f363c45c982722f63d46d7250fd8be37
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2022-06-21 20:36:47 +0200

    OK: added shedduler, tested to repeat the action every minute and it works, ..

--- cron-ping.js
+++ cron-ping.js
@@ -38,7 +38,69 @@ var sum_list = {};
 */
 
 
-fs.createReadStream('extract_caspratique.csv')
+// fs.createReadStream('extract_caspratique.csv')
+//     .pipe(csv())
+//     .on('data', (row) => {
+
+//         // this here works, but is not totaly safe : 'key" in my_object
+//         // row['LearnerId'] in sum_list ? sum_list[row['LearnerId']] += parseFloat(row['QuotedPrice']) : sum_list[row['LearnerId']] = parseFloat(row['QuotedPrice']);
+
+//         // this one is better : my_object.hasOwnProperty('key')
+//         sum_list.hasOwnProperty(row['LearnerId']) ? sum_list[row['LearnerId']] += parseFloat(row['QuotedPrice']) : sum_list[row['LearnerId']] = parseFloat(row['QuotedPrice']);
+
+//     })
+//     .on('end', () => {
+//         console.log("\n");
+//         console.log('CSV file successfully processed');
+
+//         // console.log("\n");
+//         // console.log("*************** ALL Sum List Object ***************");
+
+//         // console.log(sum_list);
+
+//         Object.entries(sum_list).forEach(entry => {
+//             const [key, value] = entry;
+//             // console.log(key, value);
+
+//             billing_list.push({
+//                 learner: key,
+//                 totalQuoted: value
+//             })
+//         });
+
+//         console.log("\n");
+//         console.log("******************** Final Billing List format ********************");
+//         console.log(billing_list);
+
+//     })
+//     .on("error", function (error) {
+//         console.log("\n");
+//         console.log(error.message);
+//     });
+
+
+
+// Execute a cron job every hour when, the minute is 01 (e.g. 19:01, 20:01, 21:01, etc.).
+const job = schedule.scheduleJob('1 * * * *', function(){
+    console.log("\n");
+    console.log('The answer to life, the universe, and everything!');
+});
+
+
+// Schedule tasks to be run on the server every hour, when the minute is 02 (e.g. 19:02, 20:02, 21:02, etc.).
+cron.schedule('2 * * * *', function() {
+    console.log("\n");
+    console.log('running a task every hour');
+});
+
+
+// Execute a cron job every 1 Minute = */1 * * * *
+const job_5 = schedule.scheduleJob('*/1 * * * *', function(){
+    console.log("\n");
+    console.log('running a task every minute');
+    console.log("\n");
+    
+    fs.createReadStream('extract_caspratique.csv')
     .pipe(csv())
     .on('data', (row) => {
 
@@ -78,17 +140,5 @@ fs.createReadStream('extract_caspratique.csv')
         console.log(error.message);
     });
 
-
-
-// Execute a cron job every hour when, the minute is 01 (e.g. 19:01, 20:01, 21:01, etc.).
-const job = schedule.scheduleJob('1 * * * *', function(){
-    console.log("\n");
-    console.log('The answer to life, the universe, and everything!');
 });
 
-
-// Schedule tasks to be run on the server every hour, when the minute is 02 (e.g. 19:02, 20:02, 21:02, etc.).
-cron.schedule('2 * * * *', function() {
-    console.log("\n");
-    console.log('running a task every hour');
-});
